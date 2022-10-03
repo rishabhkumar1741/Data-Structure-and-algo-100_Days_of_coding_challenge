2c2cadc40e03379b61d7767edb8f4aaae0bd2424
rishabhkumar <1741rishabh@gmail.com>
2022-10-03 21:39:19 +0530

    Question 5

---
+++ src/EveryDay/Question4.java
@@ -0,0 +1,42 @@
+package EveryDay;
+
+public class Question4 {
+    public static void sort012(int a[], int n)
+    {
+        int zero = 0;
+        int one =0;
+        int two = 0;
+        int index =0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            {
+                zero++;
+            }
+            else if(a[i]==1)
+            {
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        
+        for(int u=0;u<zero;u++)
+        {
+            a[index]= 0;
+            index++;
+        }
+        for(int j=0;j<one;j++)
+        {
+            a[index]= 1;
+            index++;
+        }
+        for(int z=0;z<two;z++)
+        {
+            a[index]= 2;
+            index++;
+        }
+    }
+    
+}
--- src/EveryDay/Solution.java
+++ src/EveryDay/Solution.java
@@ -1,45 +1,16 @@
 package EveryDay;
 import java.util.*;
+
+
 public class Solution {
-    public static void sort012(int a[], int n)
-    {
-        int zero = 0;
-        int one =0;
-        int two = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==0)
-            {
-                zero++;
-            }
-            else if(a[i]==1)
-            {
-                one++;
-            }
-            else{
-                two++;
-            }
-        }
-        for(int u=0;u<zero;u++)
-        {
-            System.out.print(0+" ");
-        }
-        for(int j=0;j<one;j++)
-        {
-            System.out.print(1+" ");
-        }
-        for(int z=0;z<two;z++)
-        {
-            System.out.print(2+" ");
-        }
-    }
+    
+    
 
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
          
-            int[] x = new int[]{0, 2, 1, 2, 0};
-            sort012(x, 5);
+            System.out.println("rishabh kumar");
 
     }
 }
