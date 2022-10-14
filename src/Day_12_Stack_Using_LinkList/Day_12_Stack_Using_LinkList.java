c6d1428735e96a2e1bb51eeda7ef738ab608e77f
rishabhkumar <1741rishabh@gmail.com>
2022-10-14 06:54:34 +0530

    stack using linklist

--- src/Day_12_Stack_Using_LinkList/Day_12_Stack_Using_LinkList.java
+++ src/Day_12_Stack_Using_LinkList/Day_12_Stack_Using_LinkList.java
@@ -1,27 +1,79 @@
 package Day_12_Stack_Using_LinkList;
-interface name
-{ 
-    void fun();
 
-}
-interface name1
-{ 
-    void fun1();
+import java.util.Scanner;
 
-}
-class dog{
+public class Day_12_Stack_Using_LinkList {
+    Node top = null;
 
-}
+    static class Node {
+        int data;
+        Node next;
 
-public class Day_12_Stack_Using_LinkList extends dog implements name,name1{
-    public void fun() {
-        System.out.println("rishah");
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-    public void fun1() {
-        System.out.println("rishah");
-    }
-    
 
-    
-    
+    void push(int data) {
+        Node newnode = new Node(data);
+        if (top == null) {
+            top = newnode;
+        } else {
+            newnode.next = top;
+            top = newnode;
+        }
+    }
+
+    void print() {
+        Node temp = top;
+        if (temp == null) {
+            System.out.println("stack is empty");
+        }
+        while (temp != null) {
+            System.out.print(" ==>" + temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
+
+    void pop() {
+        if (top == null) {
+            System.out.println("stack is empty ");
+        } else {
+            top = top.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Program start");
+        Scanner sc = new Scanner(System.in);
+        int choose = 1;
+        Day_12_Stack_Using_LinkList obj = new Day_12_Stack_Using_LinkList();
+        System.out.println("Choose 1 for push");
+        System.out.println("Choose 2 for pop");
+        System.out.println("Choose 3 for print");
+        System.out.println("Choose 4 for exit");
+        while (choose != 4) {
+            System.out.println("Enter your choose");
+            choose = sc.nextInt();
+            switch (choose) {
+
+                case 1:
+                    System.out.println("Enter the element you want to push");
+                    int data = sc.nextInt();
+                    obj.push(data);
+                    break;
+                case 2:
+                    obj.pop();
+                    break;
+                case 3:
+                    obj.print();
+                    break;
+
+            }
+
+        }
+    }
 }
